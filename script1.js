19cc6fd0fbb4aae54e9e5d0c55fa5e6377185b4a
praveen18111999 <124046860+praveen18111999@users.noreply.github.com>
2024-07-01 11:52:00 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<script>
+localstorage.fname="rocky";
+localstorage.adnum="454645";
+localstorage.email="prav@gmail.com";
+localstorage.password="545fgd";
+</script>
+</html>
